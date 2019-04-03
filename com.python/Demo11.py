948d83c99f2a0b950102bef4a2b0830d3187d75c
talent_mbp2018 <hl010738@126.com>
2019-04-03 17:40:37 +0800

    继承

---
+++ com.python/Demo11.py
@@ -0,0 +1,34 @@
+#继承
+
+
+class Parent:
+    def aaa(self):
+        print('Parent')
+
+
+class Par():
+    def ccc(self):
+        print('Par')
+
+
+class Sub(Parent, Par):
+    def aaa(self):
+        super(Sub, self).aaa()
+        print('Sub')
+
+
+    def bbb(self):
+        print('bbb')
+
+
+
+s = Sub()
+
+s.aaa()
+
+s.bbb()
+
+
+
+
+
